95dd6a63019044ebb71027d40643e3dfe0a011ec
test <test@test.com>
2024-06-24 18:59:36 -0400

    Added info.py to code.

---
+++ mdgeom/info.py
@@ -0,0 +1,12 @@
+"""
+Information about Simulations
+=================================
+
+This module contains functions that provide overview information about a trajectory.
+"""
+
+def summary(*universe):
+    pass
+    
+def extract(universe):
+    pass
