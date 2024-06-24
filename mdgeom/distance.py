2985c7256661930491d15f214536a0a6cd1f42fd
test <test@test.com>
2024-06-24 19:04:25 -0400

    Added distance.py

---
+++ mdgeom/distance.py
@@ -0,0 +1,19 @@
+"""
+Distance-based analysis
+=======================
+
+One of the most fundamental analysis techniques is to quantify the interactions
+between different molecules or parts of molecules by calculating distances.
+
+The use of *periodic boundary conditions* (PBC) in MD simulations can make
+distance calculations challenging. We are typically interested in distances
+that obey the **minimum image convention**, i.e., the shortest distance taking
+all periodic images into account. MDAnalysis contains powerful distance
+calculation functions that automatically apply the minimum image convention
+when supplied with the unit cell dimensions (typically named `box`, i.e., the
+lengths of the box :math:`L_x`, :math:`L_y`, and :math:`L_z` and the three
+angles :math:`alpha`, :math:`beta`, :math:`gamma`).
+"""
+
+import numpy as np
+
